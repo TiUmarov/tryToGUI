ea20907e6ac9b6e689294a191444e54eece28f9e
Umarov Timur <jojinet1@gmail.com>
2022-11-19 23:56:48 +0300

    New placing a button with relx

--- form.py
+++ form.py
@@ -13,7 +13,7 @@ place = Label(top, text = 'Press', background = 'green', fg = 'white').place(x =
 lol = Label(top, text = 'Like', background = 'green', fg = 'white').place(x = 30, y = 130)
 img = Image.open('cat2.jpg')
 img = ImageTk.PhotoImage(img)
-button = Button(top, text = 'Like', fg = 'pink', bg = 'red', height = '3', width = '5', command = lambda: top.quit()).place(x = 140, y = 480)
+button = Button(top, text = 'Like', fg = 'pink', bg = 'red', height = '3', width = '5', command = lambda: top.quit()).place(relx = '0.5', rely= '0.9')
 cat = Label(top, image = img).place(x = 20, y = 200)
 e1 = Entry(top).place(x = 90, y = 50)
 e2 = Entry(top).place(x = 90, y = 90)
